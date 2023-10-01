3dc21dddc4f7b971358efef4b2c5eea8a363c56f
tawheedshimul <tawheedshimul@gmail.com>
2023-10-02 01:04:42 +0600

    first commit

---
+++ Simplifly-Square-Root.js
@@ -0,0 +1,37 @@
+function simplifySquareRoot(number) {
+    if (number <= 1) return "√" + number;
+  
+    let sqrt = Math.floor(Math.sqrt(number));
+    let result = "";
+  
+    for (let i = 2; i <= sqrt; i++) {
+      while (number % (i * i) === 0) {
+        if (result !== "") {
+          result = 1 * i ;
+          // result += " * "
+        }
+        result += i;
+        number /= i * i;
+      }
+    }
+  
+    if (number > 1) {
+      if (result !== "") {
+        result += "";
+      }
+      result += "√" + number;
+    }
+  
+    return result !== "" ? result.replace(/\* \*/g, " * ") : "√" + number;
+  }
+  
+  // Test cases
+  console.log("√20 = " + simplifySquareRoot(20));    // Output: √20 = 2√5
+  console.log("√4 = " + simplifySquareRoot(4));      // Output: √4 = 2
+  console.log("√48 = " + simplifySquareRoot(48));    // Output: √48 = 4√3
+  console.log("√1 = " + simplifySquareRoot(1));      // Output: √1 = 1
+  console.log("√96 = " + simplifySquareRoot(96));      // Output: √9 = 3
+  console.log("√108 = " + simplifySquareRoot(108));  // Output: √108 = 6√3
+  console.log("√31 = " + simplifySquareRoot(31));    // Output: √31 = √31
+  console.log("√188 = " + simplifySquareRoot(188));    // Output : √188 = 2√47
+  console.log("√31 = " + simplifySquareRoot(31));    // Output: √31 = √31
